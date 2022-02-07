0457ecf717f1a06fdd76d649f01c54999f603f1e
SergeyHodak <4Kserge4Kserge4K@gmail.com>
2022-02-07 14:26:41 +0200

    ушел от чара пришел к интеджеру

--- src/main/java/hw10v1/Task3.java
+++ src/main/java/hw10v1/Task3.java
@@ -2,22 +2,28 @@ package hw10v1;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task3<T> {
     public String getSortedNumbers(T[] data) {
-        Optional<char[]> dats = Optional.of(Arrays.toString(data).toCharArray()); // превращение в чар в обертке опшн
-        Stream<Character> peek = new String(dats.get()).chars().mapToObj(i -> (char) i) //создать поток по каждому символу
-                .filter(Character::isDigit) // если этот чар есть цифрой
-                .sorted(); // сортировка
-
-        String result = Arrays.toString(peek.toArray()); // превратить поток в массив, а потом в строку
-        return result.substring(1, result.length()-1); // отдать без скобок
+        Optional<String> data1 = Optional.of(Arrays.toString(data)); // превращение в строку, в обертке опшн
+        String data2 = data1.get().replaceAll(",", ""); // удались все запятые
+        String data3 = data2.substring(1, data2.length()-1); // убрать первый и последний символы '[' ']'
+        String[] data4 = data3.split(" "); // разбить строку на массив слов по пробелу
+        Integer[] data5 = new Integer[data4.length]; // создать новый массив интов
+        for (int i = 0; i < data5.length; i++) { // пробежка по новому массиву
+            data5[i] = Integer.parseInt(data4[i]); // заполнить новый массив
+        }
+        String collect = Stream.of(data5) // стрим из массива цифровых значений
+                .sorted() // сортировка по возрастанию
+                .map(i -> i.toString()) // превратить инт в строку
+                .collect(Collectors.joining(", ")); // разделитель между значениями
+        return collect; // выдать результат в виде строки
     }
 
-
     public static void main(String[] args) {
-        String[] data = new String[] {"1, 2, 0", "4, 5"}; // входной массив
+        String[] data = new String[] {"1, 2, 0", "4, 10, 5, 11"}; // входной массив
         Task3<String> test = new Task3<>(); // экземпляр класса
         System.out.println(test.getSortedNumbers(data)); // вызов метода
     }
