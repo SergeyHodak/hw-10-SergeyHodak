b63ebe23cc8bb949807459e1afbca88a2696cd3c
SergeyHodak <4Kserge4Kserge4K@gmail.com>
2022-02-07 14:28:22 +0200

    реверсный компоратор

--- src/main/java/hw10v1/Task2.java
+++ src/main/java/hw10v1/Task2.java
@@ -1,6 +1,7 @@
 package hw10v1;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -8,7 +9,7 @@ public class Task2 {
     public String[] reverseSortByUpperCase(String[] data) {
         Optional<String[]> dats = Optional.of(data); // обертка в опшн
         Stream<String> result = Stream.of(dats.get()) // стрим берущий по одному объекту
-                .sorted((s1, anotherString) -> anotherString.compareTo(s1)) // реверсная сортировка
+                .sorted(Comparator.reverseOrder()) // реверсная сортировка
                 .map(String::toUpperCase); // возвести в верхний регистр
 
         return result.toArray(String[]::new); // превратить стрим в массив строк
